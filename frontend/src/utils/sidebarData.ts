4b9da399e73b04f0d8c718c16e449998fa480926
Nick Perry <nick.perry12@outlook.com>
2024-10-31 13:32:50 -0700

    Implementation: Began implementing the front end, added a side navbar and a basic home page. Began working on the `Sources` page, adding a `Create Source` button. Added some basic styling. Co-authored-by: Cruz <archzedzenrun@users.noreply.github.com>

---
+++ frontend/src/utils/sidebarData.ts
@@ -0,0 +1,20 @@
+export const sidebarData = [
+  {
+    id: 1,
+    title: 'Home',
+    path: '/',
+    className: 'nav-text'
+  },
+  {
+    id: 2,
+    title: 'Sources',
+    path: '/sources',
+    className: 'nav-text'
+  },
+  {
+    id: 3,
+    title: 'Consumers',
+    path: '/consumers',
+    className: 'nav-text'
+  }
+];
